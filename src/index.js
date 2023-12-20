85db9312f927d62ea805e1d9e27f455e5abe021f
anusha <anushaupreti99@gmail.com>
2023-12-20 22:07:47 +0545

    react starter completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hello anusha the programmer');
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css';
+
+import bg from './dreamplace.webp';
+
+const App = () => {
+  return (
+    <div>
+      <img src={bg}></img>
+      Hello dhanusha
+    </div>
+  );
+};
+
+createRoot(document.querySelector('#root')).render(<App />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,49 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/i,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|webp)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
   },
 };
